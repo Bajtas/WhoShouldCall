cd8e996a68c18dd5345d23cf643e33edd33619d7
Bartłomiej <bajtas@gmail.com>
2017-02-19 22:43:17 +0100

    Project base
    Signed-off-by: Bajtas <bajtas@gmail.com>

---
+++ src/main/java/pl/bajtas/WhoShouldRingApplication.java
@@ -0,0 +1,12 @@
+package pl.bajtas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoShouldRingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhoShouldRingApplication.class, args);
+    }
+}
