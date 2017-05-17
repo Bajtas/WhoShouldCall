1e46e845dc103c94bdd4e0e754f15b399638d8a6
Bajtas <bajtas@gmail.com>
2017-05-17 20:18:46 +0200

    Prepare for deployment, part 2.

--- src/main/java/pl/bajtas/whoshouldcall/WhoShouldCallApplication.java
+++ src/main/java/pl/bajtas/whoshouldcall/WhoShouldCallApplication.java
@@ -2,9 +2,16 @@ package pl.bajtas.whoshouldcall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = {"pl.bajtas.whoshouldcall.config"})
-public class WhoShouldCallApplication {
+public class WhoShouldCallApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WhoShouldCallApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(WhoShouldCallApplication.class, args);
     }
